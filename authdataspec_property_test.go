8ff583f260d43916813645ab6b20b874e941e85c
Hein Meling <hein.meling@uis.no>
2017-10-28 13:27:15 +0800

    added two simple property tests for the AuthDataQ quorum spec
    this adds a new dependency for the gopter package

---
+++ authdataspec_property_test.go
@@ -0,0 +1,32 @@
+package byzq
+
+import (
+	math "math"
+	"testing"
+
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
+)
+
+func TestAuthSpecProperties(t *testing.T) {
+	properties := gopter.NewProperties(nil)
+
+	properties.Property("Undefined for all <= 0", prop.ForAll(
+		func(n int) bool {
+			bq, err := NewAuthDataQ(n, priv, &priv.PublicKey)
+			return err != nil && bq == nil
+		},
+		gen.IntRange(math.MinInt32, 0),
+	))
+
+	properties.Property("3(f+1) >= n >= 3f+1", prop.ForAll(
+		func(n int) bool {
+			bq, err := NewAuthDataQ(n, priv, &priv.PublicKey)
+			return err == nil && n >= 3*bq.f+1 && n <= 3*(bq.f+1)
+		},
+		gen.IntRange(4, math.MaxInt32),
+	))
+
+	properties.TestingRun(t)
+}
